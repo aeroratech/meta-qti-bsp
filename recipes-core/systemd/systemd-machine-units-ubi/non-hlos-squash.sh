46ff738b64433773227493967745e10747aff58f
shanth kumar j p <quic_sjp@quicinc.com>
2023-06-16 15:05:47 +0530

    meta-qti-bsp: Reboot managment for firmware corruption
    firmware a corrupted switch to slot b
    when both corrupted then boot to EDL
    
    Change-Id: Ib8cb60025d2833bdda46280b0c611d1b2e3c731e

--- recipes-core/systemd/systemd-machine-units-ubi/non-hlos-squash.sh
+++ recipes-core/systemd/systemd-machine-units-ubi/non-hlos-squash.sh
@@ -40,12 +40,12 @@ IsFirmwareMounted () {
 
 GetFirmwareVolumeID () {
     firmware=$1
-    act_slot=`cat /proc/cmdline | awk -F'SLOT_SUFFIX=' '{print $2}' | awk '{print $1}' | tr -d '"'`
-    if [ "x${act_slot}" == "x" ]; then
-       act_slot="_a"
+
+    if [ "x${SLOT_SUFFIX}" == "x" ]; then
+       SLOT_SUFFIX="_a"
     fi
-    echo "active slot is $act_slot "  > /dev/kmsg
-    firmware_ab_name=${firmware}${act_slot}
+
+    firmware_ab_name=${firmware}${SLOT_SUFFIX}
     volcount=`cat ${UBI_SYS_CLASS}/volumes_count`
     vol_found=""
     for vid in `seq 0 $volcount`; do
@@ -61,6 +61,65 @@ GetFirmwareVolumeID () {
     done
     if [ "${vol_found}" == "" ]; then
        eval FindAndMountUBI modem /firmware
+    fi
+}
+
+SlotSwitchReboot () {
+    local abctl_cmd="/usr/bin/nad-abctl"
+    # Set image_set_status fields in recoveryinfo struct
+    #  'A &B' Usable     :  SET_AB_USABLE(0)
+    #  'A' corrupted     :  DONT_USE_SET_A(1)
+    #  'B' corrupted     :  DONT_USE_SET_B(2)
+    #  'A &B' corrupted  :  DONT_USE_SET_AB(3)
+
+    # Set owner fields in recoveryinfo struct
+    #  OWNER_XBL         :  1
+    #  OWNER_HLOS        :  2
+    local owner_hlos=2
+    local dont_use_set_a=1
+    local dont_use_set_ab=3
+    local firmware_a="firmware_a"
+    local firmware_b="firmware_b"
+
+    # TODO GPIO needs to be handled
+    if [ ! -e ${abctl_cmd} ]; then
+        echo "${abctl_cmd} not found, reboot to edl " > /dev/kmsg
+        /bin/sh -c 'reboot edl'
+        exit 0
+    fi
+
+    mtd_device=`cat /proc/mtd | grep recoveryinfo | awk -F ':' '{print $1}'`
+    if [ -z "${mtd_device}" ]; then
+        echo " recoveryinfo part not found, reboot to edl " > /dev/kmsg
+        /bin/sh -c 'reboot edl'
+        exit 0
+    fi
+    chmod 666 /dev/${mtd_device}
+
+    firmware_ab_name=$(cat /sys/class/ubi/ubi0_${volid}/name)
+    if [ "$firmware_ab_name" == "$firmware_a" ] || [ "$firmware_ab_name" == "$firmware_b" ] ; then
+        if [ "x${SLOT_SUFFIX}" == "x" ]; then
+            echo "SLOT_SUFFIX not present or invalid, reboot to edl" > /dev/kmsg
+            /bin/sh -c 'reboot edl'
+            exit 0
+        fi
+
+        # TODO corner case if booted with b slot needs to be handled
+        if [ "$SLOT_SUFFIX" = "_a" ]; then
+            echo "firmware A volume corrupted " > /dev/kmsg
+            ${abctl_cmd} --set_image_set_status ${dont_use_set_a}
+        else
+            echo "firmware A and B volume corrupted" > /dev/kmsg
+            ${abctl_cmd} --set_image_set_status ${dont_use_set_ab}
+        fi
+        ${abctl_cmd} --set_owner ${owner_hlos}
+        echo "Reboot for switching slots or EDL mode" > /dev/kmsg
+        /bin/sh -c 'reboot'
+    else
+        # TODO Behavior needs to be decided for AR
+        echo "non a/b volumes , reboot to edl " > /dev/kmsg
+        /bin/sh -c 'reboot edl'
+        exit 0
     fi
 }
 
@@ -111,7 +170,8 @@ FindAndMountUBIVol () {
    fi
 
    if [ $? -ne 0 ] ; then
-      echo "Unable to mount firmware volume "
+      echo "Unable to mount firmware volume " > /dev/kmsg
+      SlotSwitchReboot
       exit 0
    fi
 }
